94116045bcd66f0a5e7b70dc13dab4f7cf116b11
Jakub Man <jakubman1@gmail.com>
2019-10-20 18:08:14 +0200

    ADD styles for buttons and text

--- src/JaMP3.tsx
+++ src/JaMP3.tsx
@@ -1,18 +1,32 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import './JaMP3.scss';
-
-
-let electron = window.require('electron').remote;
-
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
 
 const JaMP3: React.FC = () => {
         //console.log(electron.dialog.showOpenDialog({ properties: ['openFile', 'openDirectory', 'multiSelections'] }));
-        let db = electron.getGlobal('db');
-        db.insert({name : "fender jazz bass", year:1977});
         return (
-            <div className={"JaMP3"}>
-                <h1>Hello db</h1>
+            <div className="JaMP3">
+                <Router>
+                    <Switch>
+                        <Route path="/home">
+                            <h1 className="logo">JaMP3</h1>
+                            <h1 className="text-center">All music</h1>
+                        </Route>
+                        <Route path="/">
+                            <h1 className="logo text-center">JaMP3</h1>
+                            <h2 className="fancy-subheading text-center">Just Another Music Player</h2>
+                            <Link to="/home" className="btn btn-big btn-outline text-center">Pokračovat</Link>
+                        </Route>
+                    </Switch>
+
+                </Router>
+
             </div>
         );
 };
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
 import JaMP3 from './JaMP3';
 import * as serviceWorker from './serviceWorker';
 
