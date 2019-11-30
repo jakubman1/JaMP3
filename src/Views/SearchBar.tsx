bd2ba1d7cf3ccb041ff4b57d744eded7c2d40a4c
Jiri-Tykva <xtykva@stud.fit.vutbr.cz>
2019-11-30 16:55:52 +0100

    ADD search bar view

--- src/Views/SearchBar.tsx
+++ src/Views/SearchBar.tsx
@@ -12,7 +12,7 @@ interface Props {
 export class SearchBar extends React.Component<Props> {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
-            <div className="search-bar-wrapper">
+            /*{<div className="search-bar-wrapper">
 
                 <select className="search-bar-menu">
                     <option value="tvoje_mama">Vše</option>
@@ -26,7 +26,24 @@ export class SearchBar extends React.Component<Props> {
                     <FontAwesomeIcon className="download-icon" icon={faDownload} />
                     <FontAwesomeIcon className="settings-icon" icon={faCog} />
                 </div>
+            </div>}*/
+
+            <div className="search-bar-wrapper">
+
+                <select className="search-bar-menu">
+                     <option value="tvoje_mama">Vše</option>
+                     <option value="tvoje_tata">Adam je pukavec</option>
+                 </select>
+
+                 <FontAwesomeIcon className="search-icon" icon={faSearch} />
+
+                 <input className="search-bar" type="text" name="searched-text" placeholder="Vyhledat..." />
+
+                 <FontAwesomeIcon className="download-icon" icon={faDownload} />
+                 <FontAwesomeIcon className="settings-icon" icon={faCog} />
+
             </div>
+
         );
     }
 }
