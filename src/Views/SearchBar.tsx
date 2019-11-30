7d4033e69538be0b423def00f474cea0d22d55a7
Jiri-Tykva <xtykva@stud.fit.vutbr.cz>
2019-11-30 19:51:11 +0100

    delete comments in search bar

--- src/Views/SearchBar.tsx
+++ src/Views/SearchBar.tsx
@@ -12,22 +12,6 @@ interface Props {
 export class SearchBar extends React.Component<Props> {
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
         return (
-            /*{<div className="search-bar-wrapper">
-
-                <select className="search-bar-menu">
-                    <option value="tvoje_mama">Vše</option>
-                    <option value="tvoje_tata">Adam je pukavec</option>
-                </select>
-
-                <FontAwesomeIcon className="search-icon" icon={faSearch} />
-                <input className="search-bar" type="text" name="searched-text" placeholder="Vyhledat..." />
-
-                <div className="search-bar-icons">
-                    <FontAwesomeIcon className="download-icon" icon={faDownload} />
-                    <FontAwesomeIcon className="settings-icon" icon={faCog} />
-                </div>
-            </div>}*/
-
             <div className="search-bar-wrapper">
 
                 <select className="search-bar-menu">
