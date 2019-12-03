51cea4ade0491b4e568e37a0fca6fcce32fb3724
Jakub Man <jakubman1@gmail.com>
2019-12-03 17:12:53 +0100

    FIX settings to display error fields

--- src/Views/Settings.tsx
+++ src/Views/Settings.tsx
@@ -11,7 +11,9 @@ export class Settings extends React.Component<IPRops> {
         togglePlay: "",
         skip: "",
         prev: "",
-        valid: true
+        toggleValid: true,
+        prevValid: true,
+        skipValid: true
     };
 
     constructor(props: IPRops) {
@@ -26,47 +28,72 @@ export class Settings extends React.Component<IPRops> {
     }
 
     validateShortcut = (shortcut: string) => {
-      return /^((CommandOrControl|Alt|Super|Shift)[+])+.|F[0-9]|F1[0-2]|Tab$/.test(shortcut);
+      return /^((CommandOrControl|Alt|Super|Shift)[+])+((.|F[0-9]|F1[0-2]|Tab)|(.|F[0-9]|F1[0-2]|Tab)[+]+)$/.test(shortcut);
     };
 
     handleSaveClick = () => {
         if(!this.validateShortcut(this.state.togglePlay)) {
-            alert(this.state.togglePlay + ' není platná klávesová zkratka :(');
-            return;
+            console.log(this.state.togglePlay + ' není platná klávesová zkratka :(');
         }
-        if(!this.validateShortcut(this.state.prev)) {
-            alert(this.state.prev + ' není platná klávesová zkratka :(');
-            return;
+        else if(!this.validateShortcut(this.state.prev)) {
+            console.log(this.state.prev + ' není platná klávesová zkratka :(');
         }
-        if(!this.validateShortcut(this.state.skip)) {
-            alert(this.state.skip + ' není platná klávesová zkratka :(');
-            return;
+        else if(!this.validateShortcut(this.state.skip)) {
+            console.log(this.state.skip + ' není platná klávesová zkratka :(');
         }
-        setShortcuts(this.state.togglePlay, this.state.prev, this.state.skip);
+        else {
+            setShortcuts(this.state.togglePlay, this.state.prev, this.state.skip);
+        }
+
     };
 
     handleToggleChange = (e: any) => {
         if(this.validateShortcut(e.target.value)) {
-            this.setState({togglePlay: e.target.value, valid: true})
+            this.setState({toggleValid: true})
         }
         else {
-            this.setState({togglePlay: e.target.value})
+            this.setState({toggleValid: false})
         }
+        this.setState({togglePlay: e.target.value});
 
     };
 
     handlePrevChange = (e: any) => {
-        this.setState({prev: e.target.value})
+        if(this.validateShortcut(e.target.value)) {
+            this.setState({prevValid: true})
+        }
+        else {
+            this.setState({prevValid: false})
+        }
+        this.setState({prev: e.target.value});
     };
 
     handleSkipChange = (e: any) => {
-        this.setState({skip: e.target.value})
+        if (this.validateShortcut(e.target.value)) {
+            this.setState({skipValid: true})
+        } else {
+            this.setState({skipValid: false})
+        }
+        this.setState({skip: e.target.value});
+    };
+
+    handleResetClick = () => {
+        this.setState({
+            togglePlay: "Alt+F6",
+            skip: "Alt+F7",
+            prev: "Alt+F5",
+            toggleValid: true,
+            prevValid: true,
+            skipValid: true
+        });
     };
 
 
     render(): React.ReactElement<any, string | React.JSXElementConstructor<any>> | string | number | {} | React.ReactNodeArray | React.ReactPortal | boolean | null | undefined {
-        let submitBtn = null;
-        if(this.state.valid) {
+        let submitBtn = (
+            <button className="btn btn-outline" onClick={this.handleSaveClick}>Neplatné zkratky</button>
+        );
+        if(this.state.toggleValid && this.state.prevValid && this.state.skipValid) {
             submitBtn = (
                 <Link to="/home" className="btn btn-outline" onClick={this.handleSaveClick}>Uložit</Link>
             );
@@ -80,27 +107,33 @@ export class Settings extends React.Component<IPRops> {
                         <span className="subtext">Výchozí Alt+F6</span>
                     </div>
                     <div>
-                        <input className="btn round-input" value={this.state.togglePlay} onChange={this.handleToggleChange} />
+                        <input type="text"
+                               className={this.state.toggleValid ? "btn round-input" : "btn round-input error-input"}
+                               value={this.state.togglePlay} onChange={this.handleToggleChange} />
                     </div>
 
                     <div>Další skladba
                         <span className="subtext">Výchozí Alt+F7</span>
                     </div>
                     <div>
-                        <input className="btn round-input" value={this.state.skip} onChange={this.handleSkipChange} />
+                        <input type="text" className={this.state.skipValid ? "btn round-input" : "btn round-input error-input"}
+                               value={this.state.skip} onChange={this.handleSkipChange} />
                     </div>
 
                     <div>Předchozí skladba
                         <span className="subtext">Výchozí Alt+F5</span>
                     </div>
                     <div>
-                        <input className="btn round-input" value={this.state.prev} onChange={this.handlePrevChange} />
+                        <input type="text" className={this.state.prevValid ? "btn round-input" : "btn round-input error-input"}
+                               value={this.state.prev} onChange={this.handlePrevChange} />
                     </div>
                     <div>
-                        <p className="error-text">Klávesy zkratek musejí být oddělené znakem + a bez mezer. Pro CTRL použijte
-                        'CommandOrControl', pro klávesu windows použijte 'Super'.</p>
+                        <button className="btn btn-outline" onClick={this.handleResetClick}>Obnovit původní nastavení</button>
+
                     </div>
                     {submitBtn}
+                    <p>Klávesy zkratek musejí být oddělené znakem + a bez mezer. Pro CTRL použijte
+                        'CommandOrControl', pro klávesu windows použijte 'Super'.</p>
                 </div>
             </div>
         );
